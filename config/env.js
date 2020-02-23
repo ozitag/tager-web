cdf1990df386c36365454ae8d252189d3bc8b25b
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-02-23 17:53:14 +0300

    Allow only env variables taht started with REACT_APP_*

--- config/env.js
+++ config/env.js
@@ -1,6 +1,21 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
+/**
+ * Only variables that started with REACT_APP_* are allowed (like in create-react-app)
+ * Discussion: https://github.com/facebook/create-react-app/issues/865#issuecomment-252199527
+ */
+const REACT_APP = /^REACT_APP_/i;
+
+function filterVariables(env) {
+  return Object.keys(env)
+    .filter(key => REACT_APP.test(key))
+    .reduce((filteredEnv, key) => {
+      filteredEnv[key] = env[key];
+      return filteredEnv;
+    }, {});
+}
+
 function getClientEnvironment() {
   const result = dotenv.config({ path: path.resolve(__dirname, '../.env') });
   if (result.error) {
@@ -11,7 +26,7 @@ function getClientEnvironment() {
       throw result.error;
     }
   } else {
-    return result.parsed;
+    return filterVariables(result.parsed);
   }
 }
 
