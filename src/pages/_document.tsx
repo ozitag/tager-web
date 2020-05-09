fec2b6af2a62c7ae68ad3ec4c7093f8fa148b838
Vital Ozierski <vital@ozerich.com>
2020-05-09 11:26:14 +0300

    add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -54,7 +54,35 @@ class CustomDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500&display=swap&subset=cyrillic"
             rel="stylesheet"
           />
-          <link rel="icon" href="/favicon-32x32.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/favicon/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/favicon/site.webmanifest" />
+          <link
+            rel="mask-icon"
+            href="/favicon/safari-pinned-tab.svg"
+            color="#dd6900"
+          />
+          <meta
+            name="msapplication-config"
+            content="/favicon/browserconfig.xml"
+          />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
           <Main />
