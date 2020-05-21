28ec6a1950ce3739070749704b5ff2faa4b31bad
Ivan Saranchonkau <i.saranchonkau@ozitag.com>
2020-05-21 15:55:36 +0300

    Add redirect map

--- server/server.ts
+++ server/server.ts
@@ -5,6 +5,7 @@ import nextI18NextMiddleware from 'next-i18next/middleware';
 
 import nextI18next from './i18n';
 import { robotsHandler, sitemapHandler } from './sitemap';
+import config from './config';
 
 function parsePort(value: string | undefined): number | null {
   if (!value) return null;
@@ -28,6 +29,17 @@ const port = parsePort(process.env.PORT) || getPortFromArgs() || 3000;
 const app = next({ dev: process.env.NODE_ENV !== 'production' });
 const handle = app.getRequestHandler();
 
+const redirectMiddleware: express.Handler = (req, res, next) => {
+  const trimmedPath = req.path.endsWith('/') ? req.path.slice(0, -1) : req.path;
+  const location = config.redirectMap.get(trimmedPath);
+
+  if (location) {
+    res.redirect(location);
+  } else {
+    next();
+  }
+};
+
 function startServer() {
   app.prepare().then(() => {
     const server = express();
@@ -44,6 +56,7 @@ function startServer() {
     server.get('/sitemap.xml', sitemapHandler);
     server.get('/robots.txt', robotsHandler);
 
+    server.use(redirectMiddleware);
     server.use(nextI18NextMiddleware(nextI18next));
 
     server.get('*', (req, res) => handle(req, res));
