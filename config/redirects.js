e977aef3193c8ac483d0f408638957da225289b0
Vital Ozierski <v.ozierski@ozitag.com>
2020-10-05 13:19:04 +0300

    remove real redirects

--- config/redirects.js
+++ config/redirects.js
@@ -1,49 +1,14 @@
-const { STORYBOOK_REDIRECT } = require('./storybook');
+const {STORYBOOK_REDIRECT} = require('./storybook');
 
 function getRedirects() {
   const isProductionServer = process.env.NEXT_PUBLIC_ENV === 'production';
 
   const redirects = [
-    {
-      source: '/page/about',
-      destination: '/about',
-      permanent: true,
-    },
-    {
-      source: '/page/rules',
-      destination: '/rules',
-      permanent: true,
-    },
-    {
-      source: '/page/terms',
-      destination: '/terms',
-      permanent: true,
-    },
-    {
-      source: '/page/doing',
-      destination: '/iz-chego-sostoit-uborka',
-      permanent: true,
-    },
-    {
-      source: '/page/career',
-      destination: '/career',
-      permanent: true,
-    },
-    {
-      source: '/profile',
-      destination: '/dashboard',
-      permanent: true,
-    },
-    {
-      source: '/profile/orders',
-      destination: '/dashboard',
-      permanent: true,
-    },
-    {
-      source: '/profile/settings',
-      destination: '/dashboard/settings',
-      permanent: true,
-    },
+    /* {
+       source: '/page/about',
+       destination: '/about',
+       permanent: true,
+     },*/
   ];
 
   if (!isProductionServer) {
